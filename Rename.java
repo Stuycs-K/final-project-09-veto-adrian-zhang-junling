37e198ca2aa7efc13c1a2a863703ee20043afcf1
Junling Zhang <jzhang40@stuy.edu>
2024-05-28 14:48:07 -0400

    started Rename Obfuscation Code

---
+++ Rename.java
@@ -0,0 +1,24 @@
+import java.util.* ;
+import java.io.* ;
+
+public class Rename{
+    public static void main(String[] args){
+        try{
+
+        }catch(Exceptions e){
+            System.out.println(e) ;
+        }
+    }
+
+    public static String variable(String line){
+        String finished = "" ;
+
+        return finished ;
+    } 
+
+    public static String functions(String line){
+        String finished = "" ;
+
+        return finished ;
+    } 
+}
