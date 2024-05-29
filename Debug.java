ef98807565496069c6de24ba00f2c4d6974bbe62
Junling Zhang <jzhang40@stuy.edu>
2024-05-29 14:19:24 -0400

    Reworked for string compantibility

--- Debug.java
+++ Debug.java
@@ -24,6 +24,46 @@ public class Debug{
 		while(scan.hasNextLine()){
 			boolean newline = true ;
 			String line = scan.nextLine() ;
+			boolean str = false ;
+			int lineComment = 0 ;
+			boolean star = false ;
+			for(char c in line){
+				if(c == '"'){
+					str = !str ;
+				}
+				if(str){
+					removed += c ;
+				}
+				if(c == '/'){
+					if(lineComment = 0){
+						lineComment = 1 ;
+					}else if(lineComment = 1){
+						lineComment = 2 ;
+						removed = removed.substring(0, removed.length() - 2) ;
+					}
+					
+				}else{
+					lineComment = 0 ;
+				}
+				if(lineComment != 2){
+					removed += c ;
+				}
+				if(lineComment == 1 && c == '*'){
+					blockComment = true ;
+				}else if(c == '/' && star){
+					blockComment = false ;
+					star = false ;
+				}
+
+				if(!blockComment){
+					removed += c ;
+				}
+				if(c == '*'){
+					star = true ;
+				}
+
+			}
+			/*
 			if(line.indexOf("/*") != -1){
 				blockComment = true ;
 				removed += line.substring(0, line.indexOf("/*")) ;
@@ -32,9 +72,9 @@ public class Debug{
 				// System.out.println(line.indexOf("/*")) ;
 				// System.out.println(line) ;
 				newline = false ;
-				if(line.indexOf("*/") != -1){
+				if(line.indexOf("*\/") != -1){
 					blockComment = false ;
-					removed += line.substring(line.indexOf("*/") + 1) ;
+					removed += line.substring(line.indexOf("*\/") + 1) ;
 					newline = true ;
 				}
 			}
@@ -47,6 +87,7 @@ public class Debug{
 			if(newline){
 				removed += "\n" ;
 			}
+			*/
 		}
 		}catch(FileNotFoundException e){
 			System.out.println("File Not Found; Check Your Input or If the file actually exists") ;
