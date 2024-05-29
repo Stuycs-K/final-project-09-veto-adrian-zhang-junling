f6fed7a7e96282c5edd423d2aa0ad9cafba0c199
Junling Zhang <jzhang40@stuy.edu>
2024-05-29 14:48:38 -0400

    Cont. working on rename obfus

--- Rename.java
+++ Rename.java
@@ -4,7 +4,15 @@ import java.io.* ;
 public class Rename{
     public static void main(String[] args){
         try{
-
+            Scanner scan = new Scanner(new File(args[0])) ;
+            String total = "" ; 
+            while(scan.hasNextLine()){
+                String line = scan.nextLine() ;
+                line = variable(line) ;
+                line = functions(line) ;
+                total += line + "\n" ;
+            }
+            FileWriter w = new FileWriter(args[0], false) ;
         }catch(Exceptions e){
             System.out.println(e) ;
         }
