f19308add122875bf3324de183a361c12d337afc
aveto40 <aveto40@stuy.edu>
2024-05-30 14:44:30 -0400

    finished caesar incorp

--- obfuscation_algs/string_obfuscation.java
+++ obfuscation_algs/string_obfuscation.java
@@ -21,5 +21,31 @@ public class string_obfuscation {
         cscanner.close();
     }
 
-    
+    private static String caesar(String text, int x) {
+        for (int i = 0; i < text.length(); i++) {
+            char oldChar = text.charAt(i);
+            if (oldChar >= 65 && oldChar <= 90) {
+                oldChar += x;
+                if (oldChar > 90) {
+                    int off = oldChar - 90;
+                    oldChar = 64;
+                    oldChar += off;
+                }
+            }
+            else if (oldChar >= 97 && oldChar <= 122) {
+                oldChar += x;
+                if (oldChar > 122) {
+                    int off = oldChar - 122;
+                    oldChar = 96;
+                    oldChar += off;
+                }
+            }
+            // System.out.println("char: " + oldChar);
+            text = text.substring(0, i) + oldChar + text.substring(i + 1, text.length());
+            // System.out.println(text);
+        }
+        // System.out.println("rotated: " + text);
+        return text;
+    }
+
 }
