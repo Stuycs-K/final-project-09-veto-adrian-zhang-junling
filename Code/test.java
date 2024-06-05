941ba80993493799e48437e43c0f61b1f56c99a2
Junling Zhang <jzhang40@stuy.edu>
2024-06-05 14:47:36 -0400

    Added sample test file

---
+++ Code/test.java
@@ -0,0 +1,19 @@
+public class test{
+    public static void main(String[] args){
+        String str0 = "// ";
+        String str1 = "/*" ;
+        // jhihh
+        int i = 0 ;
+        /*
+            Weird Secret
+        */
+       /* hihi */
+       println(str1);
+       println(str0 + "hi");
+       String help = temp(str0) ;
+    }
+
+    public static int temp(String str){
+        return str ;
+    }
+}
