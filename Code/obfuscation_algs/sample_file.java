ac7a67e3692c40b6741196801eb7887896fe2f07
aveto40 <aveto40@stuy.edu>
2024-06-07 14:19:27 -0400

    reorganization

---
+++ Code/obfuscation_algs/sample_file.java
@@ -0,0 +1,8 @@
+public class sample_file {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int i = 0;
+        System.out.println("A simple function using i: " + (i + 1));
+    }
+}
---
+++ Code/obfuscation_algs/sample_file_obfuscated.java
@@ -0,0 +1,8 @@
+public class sample_file {
+    public static void main(String[] args) {
+        System.out.println("Khoor zruog!");
+
+        int i = 0;
+        System.out.println("D vlpsoh ixqfwlrq xvlqj l: " + (i + 1));
+    }
+}
