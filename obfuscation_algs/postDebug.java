c86ae2c5c58819d1c0a18004df3994b6d8a5734a
Junling Zhang <jzhang40@stuy.edu>
2024-06-09 09:24:22 -0400

    post Debug obfuscation code

---
+++ obfuscation_algs/postDebug.java
@@ -0,0 +1,16 @@
+public class test {
+	public static void main(String[] args) {
+		String str0 = "//";
+		String str1 = "/*";
+
+		int i = 0;
+
+		println(str1);
+		println(str0 + "hi");
+		String help = temp(str0);
+	}
+
+	public static String temp(String str) {
+		return str;
+	}
+}
