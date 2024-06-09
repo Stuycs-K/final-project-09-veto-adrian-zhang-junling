9d9abd5eba65b7d5f9bce2b4badfb1203afdcfc2
Junling Zhang <jzhang40@stuy.edu>
2024-06-09 10:25:53 -0400

    Added post Rename Obfuscation code

---
+++ obfuscation_algs/postRename.java
@@ -0,0 +1,19 @@
+public class test {
+public static void main(String[] args) {
+String cplk="//";
+String hjfq="/*";
+//jhihh
+int c=0;
+/*
+* Weird Secret
+*/
+/* hihi */
+println(hjfq);
+println(cplk+"hi");
+String uqtj=bbus(cplk);
+}
+
+public static String bbus(String xnn) {
+return xnn;
+}
+}
--- obfuscation_algs/test.java
+++ obfuscation_algs/test.java
@@ -1,19 +1,19 @@
-public class test{
-    public static void main(String[] args){
-        String str0 = "// ";
-        String str1 = "/*" ;
-        // jhihh
-        int i = 0 ;
-        /*
-            Weird Secret
-        */
-       /* hihi */
-       println(str1);
-       println(str0 + "hi");
-       String help = temp(str0) ;
-    }
+public class test {
+	public static void main(String[] args) {
+		String str0 = "//";
+		String str1 = "/*";
+		// jhihh
+		int i = 0;
+		/*
+		 * Weird Secret
+		 */
+		/* hihi */
+		println(str1);
+		println(str0 + "hi");
+		String help = temp(str0);
+	}
 
-    public static int temp(String str){
-        return str ;
-    }
+	public static String temp(String str) {
+		return str;
+	}
 }
